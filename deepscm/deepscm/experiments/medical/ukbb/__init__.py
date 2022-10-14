4c413e68b22168743bc3de4f50ec5bf684a4df35
Saptarshi <saptarshi.saha_r@isical.ac.in>
2022-10-15 00:35:20 +0530

    DeepSCM has been replaced

--- deepscm/deepscm/experiments/medical/ukbb/__init__.py
+++
@@ -1,2 +0,0 @@
-# register models and experiments
-from . import sem_vi  # noqa: F401
--- deepscm/deepscm/experiments/medical/ukbb/sem_vi/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base_sem_experiment import SVIExperiment
-from .conditional_sem import ConditionalVISEM
-from .conditional_stn_sem import ConditionalSTNVISEM
-
-__all__ = [
-    'SVIExperiment',
-    'ConditionalVISEM',
-    'ConditionalSTNVISEM'
-]
